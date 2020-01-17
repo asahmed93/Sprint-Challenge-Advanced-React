c671b8389c670b50e726154b82234ffc75bfe323
asahmed93 <56940751+asahmed93@users.noreply.github.com>
2020-01-17 10:28:34 -0500

    Added tests and retrieved data

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { render, getByText } from '@testing-library/react';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('it renders', () => {
+  render(<App />)
+})
